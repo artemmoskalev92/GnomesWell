f90928d884f70bb2be14a0f18a0a99befbbf58cf
artemmoskalev92 <83223168+artemmoskalev92@users.noreply.github.com>
2022-01-28 09:09:54 +0500

    2

---
+++ GnomesWell/Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ѕреобразует данные, полученные от акселерометра,
+// в информацию о боковом смещении.
+
+public class InputManager : Singleton<InputManager>
+{
+    // ¬еличина смещени€. -1.0 = максимально влево, +1.0 = максимально вправо
+    private float _sidewaysMotion = 0.0f;
+
+    // Ёто свойство доступно только дл€ чтени€, поэтому другие сценарии не смогут изменить его.
+    public float sidewaysMotion
+    {
+        get
+        {
+            return _sidewaysMotion;
+        }
+    }
+    // ¬еличина отклонени€ сохран€етс€ в каждом кадре
+    void Update()
+    {
+        Vector3 accel = Input.acceleration;
+        _sidewaysMotion = accel.x;
+    }
+}
